75b23e74f6d7fb64667fdaefaff0ff09b4922b75
Ronal Dante Gonzales Quispe <ronal.gonzales@tecsup.edu.pe>
2026-09-05 08:57:18 -0500

    Proyecto inicial - Exploración de componentes

---
+++ app/src/main/java/com/example/lab3_exploracindecomponentes/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lab3_exploracindecomponentes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab3_exploracindecomponentes.ui.theme.LAB3_ExploraciónDeComponentesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LAB3_ExploraciónDeComponentesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LAB3_ExploraciónDeComponentesTheme {
+        Greeting("Android")
+    }
+}
